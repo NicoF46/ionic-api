b36280b28394079c42a03dab0b2b4773f5a9d888
nico <pfunes@fi.uba.ar>
2022-06-21 23:40:48 -0300

    add code for iot asignature

--- routes/medicion/index.js
+++ routes/medicion/index.js
@@ -4,7 +4,7 @@ var pool = require('../../postgres');
 
 //Espera recibir por parámetro un id de dispositivo y devuelve su última medición
 routerMedicion.get('/:idDispositivo', function(req, res) {
-    pool.query('Select * from Mediciones where dispositivoId=$1 order by fecha desc limit 1',[req.params.idDispositivo],function(err, result, fields) {
+    pool.query('Select * from Records where device_id=$1 order by created_at desc limit 1',[req.params.idDispositivo],function(err, result, fields) {
         if (err) {
             res.send(err).status(400);
             return;
@@ -16,7 +16,7 @@ routerMedicion.get('/:idDispositivo', function(req, res) {
 
 //Espera recibir por parámetro un id de dispositivo y devuelve todas sus mediciones
 routerMedicion.get('/:idDispositivo/todas', function(req, res) {
-    pool.query('Select * from Mediciones where dispositivoId=$1 order by fecha desc', [req.params.idDispositivo], function(err, result, fields) {
+    pool.query('Select * from Records where device_id=$1 order by created_at desc limit 10', [req.params.idDispositivo], function(err, result, fields) {
         if (err) {
             res.send(err).status(400);
             return;
@@ -26,7 +26,7 @@ routerMedicion.get('/:idDispositivo/todas', function(req, res) {
 });
 
 //Espera recibir por parámetro un id de dispositivo y un valor de medición y lo inserta en base de datos.
-routerMedicion.post('/', function(req, res) {
+/*routerMedicion.post('/', function(req, res) {
     console.log(req.body)
     pool.query('Insert into Mediciones (fecha,valor,dispositivoid) VALUES ($1, $2, $3)', [req.body.fecha, req.body.valor, req.body.dispositivoId], function(err, result, fields) {
         if (err) {
@@ -35,6 +35,6 @@ routerMedicion.post('/', function(req, res) {
         }
         res.send();
     });
-});
+});*/
 
 module.exports = routerMedicion;
